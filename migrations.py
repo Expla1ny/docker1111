f470a0c4cfb5371092d66dcd80b532ce1e9b481e
Expla1nyyy <103948964+Expla1ny@users.noreply.github.com>
2025-09-19 12:29:58 +0300

    i wanna go home

--- migrations.py
+++ migrations.py
@@ -15,7 +15,7 @@ class MigrationManager:
             CREATE TABLE IF NOT EXISTS clients(
                         id SERIAL PRIMARY KEY,
                         name VARCHAR(100) NOT NULL,
-                        passnum DECIMAL(10,2) NOT NULL
+                        passnum VARCHAR(20s)NULL
                         )
             ''')
         conn.commit()
